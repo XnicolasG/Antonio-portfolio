a7e05781fcfdcf1b37434a564cf3f464df4d8286
Nicolás García <nicolasg.ngl@gmail.com>
2024-10-24 20:44:07 -0500

    index page done

--- src/sections/Header/components/YearsExperience.jsx
+++ src/sections/Header/components/YearsExperience.jsx
@@ -4,27 +4,27 @@ import { motion } from 'framer-motion';
 
 export const YearsExperience = () => {
     const [number, setNumber] = useState(0);
+    const [animated, setAnimated] = useState(false);
 
-    useEffect(() => {
-        const interval = setInterval(() => {
-            setNumber((prevNumber) => {
-                if (prevNumber < 30) {
-                    return prevNumber + 1;
-                } else {
-                    return 30; // Mantén el número en 30
-                }
-            });
-        }, 70);
-
-        return () => clearInterval(interval);
-    }, []);
-
+  useEffect(() => {
+    if (!animated) {
+      let i = 0;
+      const interval = setInterval(() => {
+        i++;
+        setNumber(i);
+        if (i === 30) {
+          clearInterval(interval);
+          setAnimated(true);
+        }
+      }, 70);
+    }
+  }, [animated]);
     return (
         <motion.div
             initial={{ y: 0 }}
             animate={{ y: 0 }}
             transition={{ duration: 0.2 }}
-            className="text-5xl font-bold"
+            className="text-3xl md:text-4xl lg:text-6xl font-bold bg-gradient-to-r from-amber-500 to bg-amber-300 text-transparent inline-block bg-clip-text "
         >
             {number.toString().padStart(2, '0')}
         </motion.div>
