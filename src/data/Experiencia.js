0033cd67b92b0de256a92ee258837dfe5d8c640d
Nicolás García <nicolasg.ngl@gmail.com>
2024-10-28 17:01:20 -0500

    adding timeline

---
+++ src/data/Experiencia.js
@@ -0,0 +1,121 @@
+export const ExperienciaData = [
+    {
+        empresa: "SONDA DE COLOMBIA S.A.",
+        cargo: "Gerente de Cuenta Senior",
+        fecha: "10/2018 - 09-2024",
+        responsabilidades: [
+            { descripcion: "Gerenciamiento de cuentas, desarrollando gestión comercial en entidades del Sector Gobierno" },
+            { descripcion: "Posicionamiento y consultoría en proyectos de Tecnología Informática" },
+            { descripcion: "Presentación de Portafolio de productos y servicios de la compañía, elaborando proyectos, propuestas y licitaciones, integración de soluciones con otros Partner de negocios" },
+            { descripcion: "Posicionamiento y estructuración de proyectos de Transformación Digital para grandes entidades del estado" }
+        ],
+        logros: [
+            { descripcion: "Apertura de cuentas con oportunidades importantes como DIAN – Proyecto Nuevo Sistema Aduanero" },
+            { descripcion: "FISCALIA: Outsourcing Integral FISCATEL" }
+        ]
+    },
+    {
+        empresa: "FACTURA ELECTRONICA OnLINE",
+        cargo: "Gerente Comercial",
+        fecha: "04-17 / 10/18",
+        responsabilidades: [
+            { descripcion: "Emprendimiento propio, encargado del posicionamiento, estructuración de la unidad comercial y gestión directa abriendo nuevo mercado para el producto de Factura Electrónica" },
+            { descripcion: "Presentación de las soluciones, realización de conferencias a Sectores Empresariales diferentes, presentación y seguimiento de propuestas a clientes" }
+        ],
+        logros: [
+            { descripcion: "Diseñé y estructuré la Agencia Comercial con el objetivo de tener una participación de mercado para la nueva solución de Factura Electrónica y ofrecer al mercado en general nuestro portafolio" },
+            { descripcion: "Estructuré estrategias para presentación de portafolio de la compañía ante las empresas del Sector Privado, logrando realizar alianzas con Casas de Software para integrar nuestra solución y ofrecerlo a su portafolio de clientes" }
+        ]
+    },
+    {
+        empresa: "CONSULTING DATA SYSTEMS CDS SAS",
+        cargo: "Consultor Free Lance – Director Comercial Factura Electrónica",
+        fecha: "01-18 / 10/18",
+        responsabilidades: [
+            { descripcion: "Diseño de una estrategia de mercado para ofrecer Servicios de Factura Electrónica, realización de un plan de trabajo y una estrategia para el desarrollo de Software para conectar soluciones de Software con la plataforma de factura electrónica" },
+            { descripcion: "Coordinación de grupo comercial en las regionales y Bogotá para posicionar las soluciones con los clientes, encargado y responsable de la meta de ventas de la unidad" }
+        ],
+        logros: [
+            { descripcion: "Posicionamiento y logro de negocios importantes para la empresa en nuevos clientes" },
+            { descripcion: "Definición de una estrategia comercial para el posicionamiento de oportunidades de Factura Electrónica" }
+        ]
+    },
+    {
+        empresa: "DESCA COLOMBIA S.A.",
+        cargo: "Account Manager",
+        fecha: "10-15 / 03/17",
+        responsabilidades: [
+            { descripcion: "Encargado de realizar las labores de apertura de unidad de gobierno dentro de la compañía para poder licitar ante el estado, diagnóstico y análisis de necesidades, análisis y validación de contratación y certificación de experiencia para aval de registro único de proponentes" },
+            { descripcion: "Presentación de licitaciones, posicionamiento de portafolio de la compañía, visitas a clientes corporativos, apertura de nuevos mercados, negociación, generación de estrategias, evaluación y seguimiento" }
+        ],
+        logros: [
+            { descripcion: "Diseñé y estructuré la unidad de gobierno en la compañía con el objetivo de abrir nuevas oportunidades de negocio en el sector gobierno" },
+            { descripcion: "Posicionamiento y cierre de negocios importantes en el sector, que fueron relevantes para la empresa" }
+        ]
+    },
+    {
+        empresa: "COLVISTA SAS.",
+        cargo: "Consultor Master de Negocios",
+        fecha: "11-12 / 10/15",
+        responsabilidades: [
+            { descripcion: "Encargado de consultoría de negocios, gestión comercial para clientes del sector gobierno" },
+            { descripcion: "Presentación de licitaciones, posicionamiento de portafolio de la compañía, visitas a clientes, calificación de oportunidades" }
+        ],
+        logros: [
+            { descripcion: "Posicionamiento de la empresa en el Sector Gobierno y cierre de licitaciones y proyectos importantes para la empresa" }
+        ]
+    },
+    {
+        empresa: "VERYTEL S.A.",
+        cargo: "Gerente de Cuenta Sector Gobierno",
+        fecha: "07-09 / 09/11",
+        responsabilidades: [
+            { descripcion: "Encargado de consultoría de negocios, gestión comercial para clientes del sector gobierno" },
+            { descripcion: "Presentación de licitaciones, posicionamiento de portafolio de la compañía, visitas a clientes, calificación de oportunidades" }
+        ],
+        logros: [
+            { descripcion: "Posicionamiento de la empresa en el Sector Gobierno y cierre de licitaciones y proyectos importantes para la empresa" },
+            { descripcion: "Posicionamiento y cierre de negocios importantes en el sector, que fueron relevantes para la empresa" }
+        ]
+    },
+    {
+        empresa: "COMPUFACIL S.A.",
+        cargo: "Gerente Unidad de Infraestructura",
+        fecha: "03-07 / 12/08",
+        responsabilidades: [
+            { descripcion: "Gerenciamiento de la Unidad de Infraestructura de la compañía, encargado del Grupo comercial y Preventa, desarrollo de estrategias y gestión comercial encaminada al posicionamiento y asesoría en la Implementación y venta de proyectos de Tecnología Informática" },
+            { descripcion: "Presentación de licitaciones, posicionamiento de portafolio de la compañía, visitas a clientes, manejo de partners y fabricantes para integrar soluciones" }
+        ],
+        logros: [
+            { descripcion: "Estructuración de equipo y estrategia comercial" },
+            { descripcion: "Posicionamiento y cierre de negocios importantes en el sector, que fueron relevantes para la empresa" }
+        ]
+    },
+    {
+        empresa: "COLVISTA LTDA.",
+        cargo: "Consultor de Negocios",
+        fecha: "06-03 / 03/07",
+        responsabilidades: [
+            { descripcion: "Atención de cuentas especiales de la empresa, gerenciamiento y desarrollo de negocios en nuevos clientes del Sector Gobierno principalmente" },
+            { descripcion: "Presentación de licitaciones, posicionamiento de portafolio de la compañía, visitas a clientes, manejo de nuevos proyectos para la compañía" },
+            { descripcion: "Encargado en ausencia del Gerente Comercial de la coordinación del grupo comercial y de preventa en la asignación de proyectos y apoyo operativo y comercial" }
+        ],
+        logros: [
+            { descripcion: "Estructuración de estrategia comercial para posicionar a la empresa en sectores donde no tenía presencia" },
+            { descripcion: "Posicionamiento en el sector gobierno y cierre de negocios importantes para el crecimiento de la empresa" }
+        ]
+    },
+    {
+        empresa: "INFORMATICA DATAPOINT DE COLOMBIA LTDA.",
+        cargo: "Gerente de Cuenta Sector Gobierno",
+        fecha: "06-96 / 11/02",
+        responsabilidades: [
+            { descripcion: "Atención de cuentas especiales de la empresa, gerenciamiento y desarrollo de negocios en nuevos clientes del Sector Gobierno principalmente" },
+            { descripcion: "Presentación de licitaciones, posicionamiento de portafolio de la compañía, visitas a clientes, manejo de nuevos proyectos para la compañía" }
+        ],
+        logros: [
+            { descripcion: "Posicionamiento en cuentas grandes del sector gobierno" },
+            { descripcion: "Cierre de negocios importantes para la empresa en entidades como el Ministerio de Educación y la Secretaria de Educación del Distrito entre otros" }
+        ]
+    }
+];
