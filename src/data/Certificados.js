7c2e54324d5524c93c6be0b0089332d2df81940b
Nicolás García <nicolasg.ngl@gmail.com>
2024-10-29 18:24:40 -0500

    adding educacion component

---
+++ src/data/Certificados.js
@@ -0,0 +1,52 @@
+export const certificados = [
+    {
+        titulo: "Venta Relacional",
+        institucion: "La Enepotencia",
+        imagen: "/img/certificados/negociacion-michigan.png"
+    },
+    {
+        titulo: "Transformación Digital",
+        institucion: "Univ. Virginia",
+        imagen: "/img/certificados/negociacion-michigan.png"
+    },
+    {
+        titulo: "El Camino de la Transformación Digital",
+        institucion: "Udemy",
+        imagen: "/img/certificados/negociacion-michigan.png"
+    },
+    {
+        titulo: "Vender más y mejor en Secop II",
+        institucion: "Cámara Comercio de Bogotá",
+        imagen: "/img/certificados/negociacion-michigan.png"
+    },
+    {
+        titulo: "Diplomado de Negociación Exitosa",
+        institucion: "Universidad Michigan",
+        imagen: "/img/certificados/negociacion-michigan.png"
+    },
+    {
+        titulo: "Ciberseguridad",
+        institucion: "Sonda Academy",
+        imagen: "/img/certificados/negociacion-michigan.png"
+    },
+    {
+        titulo: "Data Center",
+        institucion: "Sonda Academy",
+        imagen: "/img/certificados/negociacion-michigan.png"
+    },
+    {
+        titulo: "Consultoría Transformacional",
+        institucion: "Sonda Academy",
+        imagen: "/img/certificados/negociacion-michigan.png"
+    },
+    {
+        titulo: "Cloud",
+        institucion: "Sonda Academy",
+        imagen: "/img/certificados/negociacion-michigan.png"
+    },
+    {
+        titulo: "Analítica",
+        institucion: "Sonda Academy",
+        imagen: "/img/certificados/negociacion-michigan.png"
+    }
+];
---
+++ src/sections/educacion/CertificationGallery.jsx
@@ -0,0 +1,66 @@
+import React, { useState } from 'react'
+import { certificados } from '../../data/Certificados';
+
+const CertificationGallery = () => {
+    const [currentIndex, setCurrentIndex] = useState(0);
+    const handleNext = () => {
+        setCurrentIndex((prevIndex) =>
+          prevIndex === 6 ? 0 : prevIndex + 1
+        );
+      };
+    
+      const handlePrev = () => {
+        setCurrentIndex((prevIndex) =>
+          prevIndex === 0 ? 6 : prevIndex - 1
+        );
+      };
+    console.log(currentIndex);
+    
+      return (
+        <section className="relative w-full flex items-center overflow-hidden">
+          <button 
+            onClick={handlePrev} 
+            className="absolute left-0 p-2 text-white bg-blue-500 rounded-l-md z-10"
+          >
+            &#9664;
+          </button>
+    
+          <div className="flex overflow-hidden w-full">
+            <ul
+              className="flex transition-transform duration-300"
+              style={{
+                transform: `translateX(-${currentIndex * 12.5}%)`
+              }}
+            >
+              {certificados.map((cert, index) => (
+                <li
+                  key={index}
+                  className="flex flex-col items-center shadow-lg p-2 w-52 h-52 flex-shrink-0 rounded bg-slate-100 text-gray-600 mr-4"
+                >
+                  <div className="w-36">
+                    <img 
+                      className="w-full object-cover"
+                      src={cert.imagen} 
+                      alt={`${cert.titulo} - ${cert.institucion}`} 
+                    />
+                  </div>
+                  <article>
+                    <p>{cert.titulo}</p>
+                    <p className="text-sm">{cert.institucion}</p>
+                  </article>
+                </li>
+              ))}
+            </ul>
+          </div>
+    
+          <button 
+            onClick={handleNext} 
+            className="absolute right-0 p-2 text-white bg-blue-500 rounded-r-md z-10"
+          >
+            &#9654;
+          </button>
+        </section>
+      );
+}
+
+export default CertificationGallery
